48f8a19db18119eeb738ad24aa0847575c869454
chlewsz <chlewsz@163.com>
2017-02-21 08:49:49 +0800

    Initial Commit

---
+++ CocoapodsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoapodsTest
+//
+//  Created by wsz on 2017/2/21.
+//  Copyright © 2017年 wsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
