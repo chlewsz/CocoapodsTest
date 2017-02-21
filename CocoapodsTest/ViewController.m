8c4f7790028f9644fcbc44c585ccd022dae156b0
chlewsz <chlewsz@163.com>
2017-02-21 09:54:28 +0800

    添加SDWebImage支持

--- CocoapodsTest/ViewController.m
+++ CocoapodsTest/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "AFNetworking.h"
+#import "UIImageView+WebCache.h"
 
 @interface ViewController ()
 
@@ -20,6 +21,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    [[UIImageView alloc] sd_setImageWithURL:[NSURL URLWithString:@"https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png"]];
 }
 
 
--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 platform :ios, '8.0'
 
 target 'CocoapodsTest' do
-pod 'AFNetworking', '~> 3.0'
+    pod 'AFNetworking', '~> 3.0'
+    pod 'SDWebImage', '~> 4.0.0'
 end
